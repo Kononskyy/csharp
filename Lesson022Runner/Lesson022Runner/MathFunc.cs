60737e71fca2d91c3e054615fb021a1d42fe3a1f
Oleksandr <memol@i.ua>
2017-02-05 22:59:44 +0200

    Add third lessonWort about Unit test

---
+++ Lesson022Runner/Lesson022Runner/MathFunc.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson022Runner
+{
+    class MathFunc
+    {
+        public int Multi(int a, int b)
+        {
+            return a * b;
+        }
+        public int Addition(int a, int b)
+        {
+            return a + b;
+        }
+        public int Division(int a, int b)
+        {
+            return a / b;
+        }
+        public int Subtraction(int a, int b)
+        {
+            return a - b;
+        }
+    }
+}
---
+++ Lesson022Runner/Lesson022Runner/MathFuncTest.cs
@@ -0,0 +1,60 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson022Runner
+{
+    [TestFixture]
+    public class MathFuncTest
+    {
+        [Test]
+        public void MultiplyTest() //Method
+        {
+            int a = 1;
+            int b = 2;
+            int expRes = 2;
+            MathFunc mathFunc = new MathFunc(); //Variable
+           
+            Assert.AreEqual(expRes, mathFunc.Multi(a, b));
+
+        }
+
+        [Test]
+        public void MultiplyTest1()
+        {
+            int a = 3;
+            int b = 3;
+            int expRes = 6;
+            MathFunc mathFunc = new MathFunc();
+
+            Assert.AreEqual(expRes, mathFunc.Addition(a, b));
+
+        }
+        [Test]
+        public void MultiplyTest2()
+        {
+            int a = 3;
+            int b = 3;
+            int expRes = 1;
+            MathFunc mathFunc = new MathFunc();
+
+            Assert.AreEqual(expRes, mathFunc.Division(a, b));
+
+        }
+        [Test]
+        public void MultiplyTest3()
+        {
+            int a = 3;
+            int b = 3;
+            int expRes = 0;
+            MathFunc mathFunc = new MathFunc();
+
+            Assert.AreEqual(expRes, mathFunc.Subtraction(a, b));
+
+        }
+    }
+
+}
